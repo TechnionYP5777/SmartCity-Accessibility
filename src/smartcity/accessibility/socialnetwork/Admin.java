89435517aa68f47464b0525911e4bfb7718d8338
Arthur Sapozhnikov <depaix3@gmail.com>
2016-12-06 19:23:41 +0200

    Added "Admin" Class for issue #20

---
+++ src/smartcity/accessibility/socialnetwork/Admin.java
@@ -0,0 +1,35 @@
+package smartcity.accessibility.socialnetwork;
+
+public class Admin implements User {
+
+	String userName;
+	String password;
+	
+	public Admin(String un, String pass){
+		userName = un;
+		password = pass;
+	}
+	
+	@Override
+	public String getUserName() {
+		return userName;
+	}
+
+	@Override
+	public String setUserName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String setPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+}
