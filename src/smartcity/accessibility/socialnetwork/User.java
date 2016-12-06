6cc0f1b7d1d129e9a7d783e65c4d7253e8afa7d1
Ariel Kolikant <a.kolikant@gmail.com>
2016-12-06 18:00:07 +0200

    added user interface

---
+++ src/smartcity/accessibility/socialnetwork/User.java
@@ -0,0 +1,15 @@
+package smartcity.accessibility.socialnetwork;
+
+/**
+ * @author Kolikant
+ *
+ */
+public interface User {
+	String getUserName();
+	
+	String setUserName();
+	
+	String setPassword();
+	
+	String getPassword();
+}
