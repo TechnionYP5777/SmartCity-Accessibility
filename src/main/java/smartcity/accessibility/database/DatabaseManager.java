ce227823da017f927a3ffa2e2c0561441b43ec96
Alexander Kaplan <alexkaplan.mail@gmail.com>
2016-12-06 12:09:14 -0800

    updated maven build path, and added jxmaps to maven dependency #23

--- src/main/java/smartcity/accessibility/database/DatabaseManager.java
+++ src/main/java/smartcity/accessibility/database/DatabaseManager.java
@@ -10,7 +10,8 @@ package smartcity.accessibility.database;
 public interface DatabaseManager {
 	final String serverUrl = "https://github.com/TechnionYP5777/SmartCity-Accessibility";
 	final String masterKey = "key";
-	
+
 	Object getValue(String clas, String key);
+
 	void putValue(String objectClass, String key, Object value);
 }
---
+++ src/test/java/smartcity/accessibility/database/DatabaseManagerTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package smartcity.accessibility.database;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author KaplanAlexander
+ *
+ */
+public class DatabaseManagerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
