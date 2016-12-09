2649f635f31a00963d29347b5441f0148ccb3a56
yaelAmitay <yaelam75@gmail.com>
2016-12-09 04:29:51 -0800

    mini skeleton of the navegation #19

---
+++ src/main/java/smartcity/accessibility/Navegation/MapSegment.java
@@ -0,0 +1,12 @@
+package smartcity.accessibility.Navegation;
+
+/**
+ * 
+ * @author yael This class represent the minimal segment of the map that can be
+ *         declared as not accessible. This class might be changed to "Street"
+ *         or something like that,
+ *
+ */
+public class MapSegment {
+
+}
---
+++ src/main/java/smartcity/accessibility/Navegation/Route.java
@@ -0,0 +1,9 @@
+package smartcity.accessibility.Navegation;
+
+/**
+ * 
+ * @author yael This class represent a route between 2 locations in the map.
+ */
+public class Route {
+
+}
