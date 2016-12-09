452a32d7f80987ae78d3e1819402722d6ae76e8f
Ariel Kolikant <a.kolikant@gmail.com>
2016-12-09 20:41:42 +0200

    #43 added functionality trying to authenticate users, still not working but on it's way

---
+++ src/main/java/smartcity/accessibility/database/UserManager.java
@@ -0,0 +1,33 @@
+package smartcity.accessibility.database;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.nimbusds.oauth2.sdk.*;
+import com.nimbusds.oauth2.sdk.id.*;
+public class UserManager {
+	
+	public AuthorizationRequest AuthorisationRequest(){
+		URI callback = null, authzEndpoint = null;
+		try {
+			authzEndpoint = new URI("http://aaasdasdasd.wixsite.com/mysite");
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		ClientID clientID = new ClientID("123");
+		
+		Scope scope = new Scope("read", "write");
+		
+		try {
+			callback = new URI("https://client.com/callback");
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new AuthorizationRequest.Builder(new ResponseType(ResponseType.Value.CODE), clientID).scope(scope)
+				.state((new State())).redirectionURI(callback).endpointURI(authzEndpoint).build();
+	}
+	
+	
+}
