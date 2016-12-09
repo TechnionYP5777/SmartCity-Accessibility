317e6ff804c258af87297cd06b8f7310b48d03e0
Alexander Kaplan <alexkaplan.mail@gmail.com>
2016-12-09 02:16:36 -0800

    added new maven repo to get pre-release version of parse4j, and initilized server #33

--- src/main/java/smartcity/accessibility/database/DatabaseManager.java
+++ src/main/java/smartcity/accessibility/database/DatabaseManager.java
@@ -1,14 +1,25 @@
 package smartcity.accessibility.database;
 
+import org.parse4j.Parse;
+
 /**
  * @author KaplanAlexander
  *
  */
-public interface DatabaseManager {
-	final String serverUrl = "https://github.com/TechnionYP5777/SmartCity-Accessibility";
-	final String masterKey = "key";
+public abstract class DatabaseManager {
+	public static final String serverUrl = "https://github.com/TechnionYP5777/SmartCity-Accessibility";
+	public static final String masterKey = "0ef32e07-dec4-4ab0-cfdf4-a2628bd39";
+	public static final String appId = "smartcityaccessibility";
+	
+	public static void initialize(){
+		Parse.initialize(appId, masterKey, serverUrl);
+	}
+	
+	public Object getValue(final String clas, final String key) {
+		return null;
+	}
 
-	Object getValue(String clas, String key);
+	public void putValue(final String objectClass, final String key, final Object value) {
 
-	void putValue(String objectClass, String key, Object value);
+	}
 }
