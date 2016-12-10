b41f73615cad7af5f45b50fea724b8b4e837d434
Ariel Kolikant <a.kolikant@gmail.com>
2016-12-10 12:00:14 +0200

    added make key method

--- src/main/java/smartcity/accessibility/database/UserManager.java
+++ src/main/java/smartcity/accessibility/database/UserManager.java
@@ -8,8 +8,15 @@ public class UserManager {
 	
 	private final char seperator = '%';
 	
+	private String makeKey(String name, String password){
+		return name+seperator+password;
+	}
+	
 	public boolean Authenticate(String name, String password){
+		String key = makeKey(name, password);
+		
 		return false;
 	}
+	
 	
 }
