c656e455fce25e3aaf3f6e1bff0d1a8526a6beac
Arthur Sapozhnikov <depaix3@gmail.com>
2016-12-12 18:07:56 +0200

    Edited and added stubs according to 12/12/16 meeting. Cant implement yet

--- src/main/java/smartcity/accessibility/socialnetwork/Admin.java
+++ src/main/java/smartcity/accessibility/socialnetwork/Admin.java
@@ -1,5 +1,6 @@
 package smartcity.accessibility.socialnetwork;
 
+import smartcity.accessibility.database.ReviewManager;
 
 /**
  * @author ArthurSap
@@ -8,16 +9,26 @@ package smartcity.accessibility.socialnetwork;
 
 public class Admin extends AuthenticatedUser {
 
-
 	public Admin(String un, String pass){
 		super(un,pass);
 		//TODO more stuff on Admin
 	}
 	
-	public void treatReview(Review r){
-		//TODO once decided on github, implement the way
-		//reviews should get special treatment by the admin.
-		//issue #48
+	
+	/**
+	 * Marks a review as important - whilst calculating the location's 
+	 * accessibility level always takes this review in the calculation.
+	 * Also, always show this review in the top reviews.
+	 **/
+	public void pinReview(Review r){
+		//TODO issue #48
+	}
+	
+	/**
+	 * Reverts the effects of pinReview.
+	 */
+	public void unpinReview(Review r){
+		//TODO issue #48
 	}
 	
 	public void deleteReview(Review r){
@@ -25,7 +36,14 @@ public class Admin extends AuthenticatedUser {
 		//issue #32
 	}
 	
-
+	/**
+	 * Uploads Admins' reviews. 
+	 * Pins them automatically.
+	 */
+	public void uploadReview(Review r){
+		ReviewManager.uploadReview(r, this);
+		pinReview(r);
+	}
 	
 
 	@Override
