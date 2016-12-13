cd08e3b63a55deead2e80eb3622e7f76d5967767
yaelAmitay <yaelam75@gmail.com>
2016-12-12 20:58:23 -0800

    add key to navigation #64

--- src/main/java/smartcity/accessibility/navigation/Navigation.java
+++ src/main/java/smartcity/accessibility/navigation/Navigation.java
@@ -12,6 +12,8 @@ import smartcity.accessibility.navigation.mapquestcommunication.Route;
  */
 public class Navigation {
 
+	final String mapquestKey = "oa35ASKtNrnBGwF1cW0xV6fXujJ81j2Y";
+
 	public Navigation() {
 	}
 
@@ -20,28 +22,32 @@ public class Navigation {
 	 */
 	public Route showRoute(Location source, Location destination, Integer accessabilityThreshold) {
 		Double radiusOfRoute = calcRadius(source, destination);
-		List<MapSegment> segmentsToAvoid = getSegmentsToAvoid(source, destination, accessabilityThreshold,radiusOfRoute);
+		List<MapSegment> segmentsToAvoid = getSegmentsToAvoid(source, destination, accessabilityThreshold,
+				radiusOfRoute);
 		@SuppressWarnings("unused")
 		Route route = getRouteFromMapQuest(segmentsToAvoid);
-		//TODO display route for now
-		//TODO convert route to a format that can be displayed in JXMap
+		// TODO display route for now
+		// TODO convert route to a format that can be displayed in JXMap
 		return null;
 	}
 
 	private Route getRouteFromMapQuest(List<MapSegment> segmentsToAvoid) {
-		//(1) TODO request from server to avoid the segments of segmentsToAvoid
-		//(2) TODO request route form servers
+		// (1) TODO request from server to avoid the segments of segmentsToAvoid
+		// (2) TODO request route form servers
 		return null;
 	}
 
 	private Double calcRadius(Location source, Location destination) {
-		// TODO calculate the radius of the circle which the route can pass through.
+		// TODO calculate the radius of the circle which the route can pass
+		// through.
 		return null;
 	}
 
-	private List<MapSegment> getSegmentsToAvoid(Location source, Location destination, Integer accessabilityThreshold, Double radiusOfRoute) {
+	private List<MapSegment> getSegmentsToAvoid(Location source, Location destination, Integer accessabilityThreshold,
+			Double radiusOfRoute) {
 		// TODO return list of segment to avoid within the given radius
-		//this method will use LocationManager to get the locations and then identify their MapSegment
+		// this method will use LocationManager to get the locations and then
+		// identify their MapSegment
 		return null;
 	}
 
