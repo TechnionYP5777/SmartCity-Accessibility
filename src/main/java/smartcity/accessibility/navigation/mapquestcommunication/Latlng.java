83ffd41f23aeb8a6b2db3bbfe9556451509e695f
yaelAmitay <yaelam75@gmail.com>
2016-12-17 12:45:53 -0800

    add latlng class #54

---
+++ src/main/java/smartcity/accessibility/navigation/mapquestcommunication/Latlng.java
@@ -0,0 +1,30 @@
+package smartcity.accessibility.navigation.mapquestcommunication;
+
+/**
+ * 
+ * @author yael This class represent lat and lng of a location.
+ */
+public class Latlng {
+	private Long lat;
+	private Long lng;
+
+	public Latlng() {
+	}
+
+	public Long getLat() {
+		return lat;
+	}
+
+	public void setLat(Long lat) {
+		this.lat = lat;
+	}
+
+	public Long getLng() {
+		return lng;
+	}
+
+	public void setLng(Long lng) {
+		this.lng = lng;
+	}
+
+}
