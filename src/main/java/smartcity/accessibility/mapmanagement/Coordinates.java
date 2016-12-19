22ad2cae2223c896cb4de3c039481666d24a6968
KoralChapnik <chapnikkoral@gmail.com>
2016-12-19 06:18:13 -0800

    added class Coordinates #58

---
+++ src/main/java/smartcity/accessibility/mapmanagement/Coordinates.java
@@ -0,0 +1,17 @@
+package smartcity.accessibility.mapmanagement;
+
+/**
+ * 
+ * @author Koral Chapnik
+ * This class encapsulates JxMaps LatLng
+ *
+ */
+public class Coordinates extends Location {
+
+	@Override
+	public String getAddress() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
