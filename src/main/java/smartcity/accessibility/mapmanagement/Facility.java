cb68509ff829bbf469b70d5ef70142a540beabba
koralchapnik <chapnikkoral@gmail.com>
2016-12-23 17:42:31 +0200

    added Facility class #58

---
+++ src/main/java/smartcity/accessibility/mapmanagement/Facility.java
@@ -0,0 +1,12 @@
+package smartcity.accessibility.mapmanagement;
+
+/**
+ * 
+ * @author Koral Chapnik
+ *
+ */
+public class Facility extends Location{
+
+	private static final long serialVersionUID = -6523278702704086341L;
+
+}
