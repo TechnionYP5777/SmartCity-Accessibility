d904db348c25f6e57f0a800490291f871b3ad2f5
koralchapnik <chapnikkoral@gmail.com>
2016-12-27 08:11:17 +0200

    added constructor to Coordinates #58

--- src/main/java/smartcity/accessibility/mapmanagement/Coordinates.java
+++ src/main/java/smartcity/accessibility/mapmanagement/Coordinates.java
@@ -1,5 +1,7 @@
 package smartcity.accessibility.mapmanagement;
 
+import com.teamdev.jxmaps.LatLng;
+
 /**
  * 
  * @author Koral Chapnik
@@ -9,4 +11,7 @@ package smartcity.accessibility.mapmanagement;
 public class Coordinates extends Location {
 	private static final long serialVersionUID = -5647589570659349011L;
 
+	public Coordinates(LatLng c) {
+		super(c);
+	}
 }
