eeb18d9e2969ac30d429c74a6877c56729ed0cd3
Arthur Sapozhnikov <depaix3@gmail.com>
2016-12-29 13:22:07 +0200

    Added a test to check basic user functionalities

---
+++ src/test/java/smartcity/accessibility/socialnetwork/UserFunctionalityTest.java
@@ -0,0 +1,55 @@
+package smartcity.accessibility.socialnetwork;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import smartcity.accessibility.socialnetwork.User.Privilege;
+
+public class UserFunctionalityTest {
+
+	@Test
+	public void userNameFunctionality() { 
+		User u = UserImpl.RegularUser("RegularUser", "", "");
+		assertEquals("RegularUser", u.getName());
+		
+		u.setName("Name has chnged");
+		assertEquals("Name has chnged", u.getName());
+	}
+	
+	@Test
+	public void passwordFunctionality() { 
+		User u = UserImpl.RegularUser("User", "123", "");
+		assertEquals("123", u.getPassword());
+		
+		u.setPassword("123456");
+		assertEquals("123456", u.getPassword());
+	}
+	
+	@Test
+	public void staticConstructorsCorrectness() {  
+		
+		assertEquals(Privilege.DefaultUser,
+				UserImpl.DefaultUser("DefaultUser", "", "").getPrivilege());
+		
+		assertEquals(Privilege.RegularUser,
+				UserImpl.RegularUser("RegularUser", "", "").getPrivilege());
+		
+		assertEquals(Privilege.Admin,
+				UserImpl.Admin("Admin", "", "").getPrivilege());
+	}
+	
+	@Test
+	public void privilegeCorrectness() { 
+	
+		User u = UserImpl.DefaultUser("DefaultUser", "", "");
+		assertEquals(Privilege.DefaultUser, u.getPrivilege());
+		
+		u.setPrivilege(Privilege.RegularUser);
+		assertEquals(Privilege.RegularUser, u.getPrivilege());
+		
+		u.setPrivilege(Privilege.Admin);
+		assertEquals(Privilege.Admin, u.getPrivilege());
+	}
+
+}
