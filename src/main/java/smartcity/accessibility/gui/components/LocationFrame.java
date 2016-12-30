0cd32e3c5ec8b0a5d1576596f40bb8d5fdf60423
Alexander Kaplan <alexkaplan.mail@gmail.com>
2016-12-30 07:25:44 -0800

    created location frame to be used on click #29

--- src/main/java/smartcity/accessibility/gui/components/LocationFrame.java
+++ src/main/java/smartcity/accessibility/gui/components/LocationFrame.java
@@ -1,35 +1,37 @@
 package smartcity.accessibility.gui.components;
 
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
+import com.teamdev.jxmaps.LatLng;
+
+import smartcity.accessibility.database.LocationManager;
+import smartcity.accessibility.mapmanagement.Location;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class LocationFrame {
 
 	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					LocationFrame window = new LocationFrame();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+	private Location loc;
+	
+	public static void main(String[] args) throws InterruptedException{
+		LocationFrame f =new LocationFrame(new LatLng());
+		//f.wait();
 	}
 
 	/**
 	 * Create the application.
 	 */
-	public LocationFrame() {
+	public LocationFrame(LatLng loc) {
+		this.loc = LocationManager.getLocation(loc);
+		//this.loc.getReviews().get(0).
 		initialize();
 	}
 
@@ -45,6 +47,22 @@ public class LocationFrame {
 		JLabel lblLocation = new JLabel("Location");
 		lblLocation.setBounds(182, 28, 70, 15);
 		frame.getContentPane().add(lblLocation);
+		
+		JPanel jp = new JPanel();
+		jp.setLayout(new GridLayout(0,1));
+		for(int i=0;i<100;i++){
+			JButton temp = new JButton("a");
+			temp.setVisible(true);
+			
+			jp.add(temp);
+		}
+		
+		JScrollPane scrollPane = new JScrollPane(jp);
+		
+		scrollPane.setBounds(12, 50, 411, 167);
+		scrollPane.setVisible(true);
+		frame.getContentPane().add(scrollPane);
+		
+		frame.setVisible(true);
 	}
-
 }
