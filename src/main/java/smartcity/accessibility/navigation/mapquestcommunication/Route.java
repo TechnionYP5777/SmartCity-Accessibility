04d7e4f1ae81cebe6475338a0679527e5ea6e53f
yaelAmitay <yaelam75@gmail.com>
2017-01-12 01:39:45 +0200

    delete class that are not relevant anymore for navigation

--- src/main/java/smartcity/accessibility/navigation/mapquestcommunication/Route.java
+++ src/main/java/smartcity/accessibility/navigation/mapquestcommunication/Route.java
@@ -1,23 +1,14 @@
 package smartcity.accessibility.navigation.mapquestcommunication;
 
 /**
- * 
- * @author yael This class represent a route between 2 locations in the map.
+ * This class represent a route between 2 locations in the map.
+ * @author yael 
  */
 public class Route {
-	private Leg[] legs;
 	private Shape shape;
 
 	public Route() {
 
-	}
-
-	public Leg[] getLegs() {
-		return legs;
-	}
-
-	public void setLegs(Leg[] ls) {
-		this.legs = ls;
 	}
 
 	public Shape getShape() {
