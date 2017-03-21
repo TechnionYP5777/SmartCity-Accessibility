3f20ad375da611bfea3c40de38c2e3b0cfd48773
Alex Kaplan <alexkaplan.mail@gmail.com>
2017-03-21 19:46:50 +0200

    moved to artifactory server

--- src/test/java/smartcity/accessibility/navigation/NavigationTest.java
+++ src/test/java/smartcity/accessibility/navigation/NavigationTest.java
@@ -3,6 +3,7 @@ package smartcity.accessibility.navigation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
@@ -40,6 +41,7 @@ public class NavigationTest {
 		System.out.println(m.getStreet());
 	}
 
+	
 	@Test
 	public void avoidOneSegement() throws CommunicationFailed {
 		Latlng from = new Latlng(31.768762, 34.632052), to = new Latlng(31.770981, 34.620567);
