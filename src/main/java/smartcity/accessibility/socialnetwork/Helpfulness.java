dfe6479baf7514222a70a2a5267a63339c242729
koralchapnik <chapnikkoral@gmail.com>
2017-04-04 01:28:00 +0300

    commit before merging

--- src/main/java/smartcity/accessibility/socialnetwork/Helpfulness.java
+++ src/main/java/smartcity/accessibility/socialnetwork/Helpfulness.java
@@ -3,14 +3,14 @@ package smartcity.accessibility.socialnetwork;
 /**
  * 
  * @author Koral Chapnik
- * This class aimed to calculate the user's helpfulness 
+ * This class aimed to calculate the user's helpfulness in choosing the best reviews
  *
  */
 public class Helpfulness {
-
+	
 	//a stub
-	int getHelpfulness() {
-		//to implement
+	int getHelpfulness(User u) {
+		
 		return 0;
 	}
 }
