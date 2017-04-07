091def9844b02915bb024298dfa1303abf8cdb2a
Ariel Kolikant <a.kolikant@gmail.com>
2017-04-07 18:44:16 +0300

    search now understand the search query that it is given. need for it to be given searchQueries as the parzed string that they are and split it to the search itself next #258

--- server-side/src/main/java/smartcity/accessibility/services/SimpleSearchService.java
+++ server-side/src/main/java/smartcity/accessibility/services/SimpleSearchService.java
@@ -1,12 +1,15 @@
 package smartcity.accessibility.services;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleSearchService {
-	@RequestMapping("/simpleSearch")
-    public String test() {
-    	return "Omg So Much Searching added successfuly";
+	@RequestMapping(value="/simpleSearch/{search}")
+	@ResponseBody
+    public String searchService(@PathVariable("search") String search) {
+    	return "a, I see you are searching for " + search + " well....tough luck";
     }
 }
