9e81da32827253d381c6c663a1a821595391daf4
Alex Kaplan <alexkaplan.mail@gmail.com>
2017-04-08 14:29:50 +0300

    removed redundant lines from guice module

--- server-side/src/main/java/smartcity/accessibility/database/DatabaseModule.java
+++ server-side/src/main/java/smartcity/accessibility/database/DatabaseModule.java
@@ -6,7 +6,5 @@ public class DatabaseModule extends AbstractModule {
 	@Override
 	protected void configure() {
 		bind(Database.class).toInstance(ParseDatabase.get());
-		bind(ReviewManager.class).toInstance(new ReviewManager(ParseDatabase.get()));
-		bind(LocationManager.class).toInstance(new LocationManager(ParseDatabase.get()));
 	}
 }
