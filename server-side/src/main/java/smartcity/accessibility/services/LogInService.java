7c2392f34a0221e2ac7a0084148f625674e03220
yaelAmitay <yaelam75@gmail.com>
2017-04-08 21:06:12 +0300

    add method to check if user logged in #253

--- server-side/src/main/java/smartcity/accessibility/services/LogInService.java
+++ server-side/src/main/java/smartcity/accessibility/services/LogInService.java
@@ -1,11 +1,12 @@
 package smartcity.accessibility.services;
 
+import java.util.concurrent.ExecutionException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import smartcity.accessibility.socialnetwork.User;
 import smartcity.accessibility.socialnetwork.UserImpl;
-
 
 @Controller
 public class LogInService {
@@ -13,8 +14,22 @@ public class LogInService {
 	@RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json")
 	@ResponseBody
 	public Integer login(@RequestParam("name") String name, @RequestParam("password") String password) {
-		User u = UserImpl.RegularUser(name,password,"");
+		User u = UserImpl.RegularUser(name, password, "");
 		Application.tokenToSession.put(u.hashCode(), new UserInfo(u));
 		return u.hashCode();
+	}
+
+	static boolean isUserLoggedIn(User u) {
+		Integer token = u.hashCode();
+		UserInfo userInfo = null;
+		try {
+			userInfo = Application.tokenToSession.get(token);
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		if (userInfo.getUser() == null) {
+			return false;
+		}
+		return true;
 	}
 }
