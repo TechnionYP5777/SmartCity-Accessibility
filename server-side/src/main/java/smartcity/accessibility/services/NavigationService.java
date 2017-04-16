17e2765c295144847f0be1a1434f7fff42f730b2
yaelAmitay <yaelam75@gmail.com>
2017-04-16 04:32:49 +0300

    Navigation get token from header #277

--- server-side/src/main/java/smartcity/accessibility/services/NavigationService.java
+++ server-side/src/main/java/smartcity/accessibility/services/NavigationService.java
@@ -3,18 +3,18 @@ package smartcity.accessibility.services;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Controller
 public class NavigationService {
 	@RequestMapping(value = "/navigation", method = RequestMethod.POST, produces = "application/json")
 	@ResponseBody
-	public Integer login(@RequestParam("token") String token) {
+	public String login(@RequestHeader("authToken") String token) {
 		if (LogInService.isUserLoggedIn(token)) {
 			throw new UserIsNotLoggedIn();
 		}
-		return 0;
+		return "work";
 	}
 
 }
