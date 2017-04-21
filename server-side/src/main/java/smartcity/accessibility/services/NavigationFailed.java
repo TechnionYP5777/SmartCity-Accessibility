a337a6cbf1b7d0d191186692187923e00775bc0b
yaelAmitay <yaelam75@gmail.com>
2017-04-21 11:49:11 +0300

    forgot to add file

---
+++ server-side/src/main/java/smartcity/accessibility/services/NavigationFailed.java
@@ -0,0 +1,26 @@
+package smartcity.accessibility.services;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class NavigationFailed extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	private String message;
+
+	public NavigationFailed(String message) {
+		this.message = message;
+	}
+	
+	public NavigationFailed() {
+		this.message = "We could not find the cause of the problem :(";
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
