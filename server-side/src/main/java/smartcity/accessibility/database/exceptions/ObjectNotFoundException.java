a83fc3b5611c3cece3d3f0f971d277e6c4eace0f
Alex Kaplan <alexkaplan.mail@gmail.com>
2017-04-27 20:47:20 +0300

    added exception to database interface #295 for real

---
+++ server-side/src/main/java/smartcity/accessibility/database/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package smartcity.accessibility.database.exceptions;
+
+public class ObjectNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5556963917061085389L;
+
+}
