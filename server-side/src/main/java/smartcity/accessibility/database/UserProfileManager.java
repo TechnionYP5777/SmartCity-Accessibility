bf78c182d57e682d24f78f5d7f4fab87f4251419
Alex Kaplan <alexkaplan.mail@gmail.com>
2017-04-27 20:50:39 +0300

    remove warnings

--- server-side/src/main/java/smartcity/accessibility/database/UserProfileManager.java
+++ server-side/src/main/java/smartcity/accessibility/database/UserProfileManager.java
@@ -3,7 +3,6 @@ package smartcity.accessibility.database;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
