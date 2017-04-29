39d64b9ab569d9211fe02a1767dc0cc9c90b8d40
Arthur Sapozhnikov <depaix3@gmail.com>
2017-04-29 16:11:45 +0300

    Implemented basic server side of add review. Need to wait for DBs to be implemented and add a request for user credentials.

--- server-side/src/main/java/smartcity/accessibility/services/AddReviewService.java
+++ server-side/src/main/java/smartcity/accessibility/services/AddReviewService.java
@@ -1,11 +1,22 @@
 package smartcity.accessibility.services;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import smartcity.accessibility.socialnetwork.Review;
 
 @RestController
 public class AddReviewService {
-	@RequestMapping("/reviews")
-    public String test() {
-    	return "Review added successfuly";
+	@RequestMapping(value = "/addreview", method = RequestMethod.POST,
+			produces = "application/json")
+	@ResponseBody
+    public void addreview(@RequestParam("review") String review,
+    		@RequestParam("score") String score) {
+		//TODO receive user credentials
+		Review r = new Review(Integer.parseInt(score), review, null);
+		//TODO wait until alex finishes DBs and upload the review
+		//TODO pretty pretty finisher    	
     }
 }
