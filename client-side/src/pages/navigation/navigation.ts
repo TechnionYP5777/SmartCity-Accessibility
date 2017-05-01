b42d6d7ec107147ba90fe4192e818a3a168b10c2
yaelAmitay <yaelam75@gmail.com>
2017-05-01 20:13:52 +0300

    add src location to navigation 276

--- client-side/src/pages/navigation/navigation.ts
+++ client-side/src/pages/navigation/navigation.ts
@@ -4,6 +4,7 @@ import { NavigationService } from './navigationService';
 import {MapviewPage} from '../mapview/mapview';
 import { LoginService } from '../login/LoginService';
 import { UserPagePage } from '../user-page/user-page';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-navigation',
@@ -12,10 +13,20 @@ import { UserPagePage } from '../user-page/user-page';
 export class NavigationPage {
     isWork : any;
     isLoggedin : any;
+	geolocation: Geolocation;
+	srcLocation = {
+		lat : '',
+		lng : ''
+	};
     constructor(public navCtrl: NavController, public navParams: NavParams, public navigationService: NavigationService,public loginService : LoginService) {
 	    var token = window.sessionStorage.getItem('token');
 		this.isWork = token;
 		this.isLoggedin = this.loginService.isLoggedIn();
+		this.geolocation = new Geolocation();
+		this.geolocation.getCurrentPosition().then((position) => {
+			this.srcLocation.lat = String(position.coords.latitude);
+			this.srcLocation.lng = String(position.coords.longitude);
+		});
 	}
 
 	startNavigation(){
