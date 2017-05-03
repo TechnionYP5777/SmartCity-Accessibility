8cb99bdf4fee6f032d736982d6c9900ee9e8e366
Arthur Sapozhnikov <depaix3@gmail.com>
2017-05-03 14:43:47 +0300

    Extracted user object from the token.

--- server-side/src/main/java/smartcity/accessibility/services/AddReviewService.java
+++ server-side/src/main/java/smartcity/accessibility/services/AddReviewService.java
@@ -1,4 +1,6 @@
 package smartcity.accessibility.services;
+import java.util.concurrent.ExecutionException;
+
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import smartcity.accessibility.services.exceptions.UserIsNotLoggedIn;
 import smartcity.accessibility.socialnetwork.Review;
+import smartcity.accessibility.socialnetwork.User;
 
 @RestController
 public class AddReviewService {
@@ -18,12 +21,25 @@ public class AddReviewService {
     		@RequestParam("review") String review,
     		@RequestParam("score") String score) {
 		
-		if (!LogInService.isUserLoggedIn(token)) {
+		if (!LogInService.isUserLoggedIn(token))
 			throw new UserIsNotLoggedIn();
-		}
 		
-		Review r = new Review(Integer.parseInt(score), review, null);
+		User u = getUserFromToken(token);
+		if (u == null)
+			throw new UserIsNotLoggedIn();
+		
+		Review r = new Review(Integer.parseInt(score), review, u);
 		//TODO wait until alex finishes DBs and upload the review
 		//TODO pretty pretty finisher    	
     }
+	
+	private User getUserFromToken(String token){
+		UserInfo userInfo = null;
+		try {
+			userInfo = Application.tokenToSession.get(token);
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		return userInfo.getUser();
+	}
 }
