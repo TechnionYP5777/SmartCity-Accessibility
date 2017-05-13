df4eff1d28b9b60fc0eba51f3970cb09782061b4
yaelAmitay <yaelam75@gmail.com>
2017-05-14 00:06:15 +0300

    add mocking and category to LoginService Test #316

--- server-side/src/test/java/smartcity/accessibility/services/LoginServiceTest.java
+++ server-side/src/test/java/smartcity/accessibility/services/LoginServiceTest.java
@@ -4,10 +4,16 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import smartcity.accessibility.categories.UnitTests;
+import smartcity.accessibility.database.AbstractUserProfileManager;
+import smartcity.accessibility.database.ParseDatabase;
 
 /**
  * @author yael
@@ -15,9 +21,13 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @WebAppConfiguration
+@Category(UnitTests.class)
 public class LoginServiceTest extends ServiceTest {
 	@Before
 	public void setup() throws Exception {
+		ParseDatabase.initialize();
+		AbstractUserProfileManager mock_UserManagerProfile = Mockito.mock(AbstractUserProfileManager.class);
+		AbstractUserProfileManager.initialize(mock_UserManagerProfile);
 		this.mockMvc = webAppContextSetup(webApplicationContext).build();
 	}
 	
