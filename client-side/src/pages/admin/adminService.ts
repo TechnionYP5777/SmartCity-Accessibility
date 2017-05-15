7f3f2a24fa493582c30662bbde1e0ab62c7ac549
koralchapnik <chapnikkoral@gmail.com>
2017-05-15 15:33:10 +0300

    using the admin service #322

--- client-side/src/pages/admin/adminService.ts
+++ client-side/src/pages/admin/adminService.ts
@@ -1,5 +1,6 @@
 import {Injectable} from "@angular/core";
 import { Http, Headers,RequestOptions } from "@angular/http";
+import { Constants } from "../constants";
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -7,5 +8,13 @@ export class AdminService {
     constructor(public http: Http) {
         this.http = http;
     }
+
+	getUserProfile() {
+		var token = window.sessionStorage.getItem('token');
+		var headers = new Headers();
+		headers.append('Content-Type', 'application/x-www-form-urlencoded');
+		headers.append('authToken',token);
+		return this.http.get(Constants.serverAddress +'/adminInfo', {headers: headers}).map(res=>res.json());
+	}
 
 }
