7f4cd3ce960c1921724e0570c95e56885dd0b1a5
yaelAmitay <yaelam75@gmail.com>
2017-05-17 23:03:50 +0300

    sonar and logger #331

--- server-side/src/main/java/smartcity/accessibility/services/Token.java
+++ server-side/src/main/java/smartcity/accessibility/services/Token.java
@@ -4,30 +4,37 @@ import smartcity.accessibility.socialnetwork.User;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author yael
  */
 public class Token {
-	private String token;
-	private Token(String token) {
-		this.setToken(token);
-	}
-	static public Token calcToken(User u){
-		String str = u.getUsername()+u.getPassword();
-		MessageDigest messageDigest = null;
+
+	private static Logger logger = LoggerFactory.getLogger(Token.class);
+	private String tokenString;
+
+	public static Token calcToken(User u) {
+		String str = u.getUsername() + u.getPassword();
+		Token t = new Token();
 		try {
-			messageDigest = MessageDigest.getInstance("SHA-256");
+			MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+			messageDigest.update(str.getBytes());
+			String hashStr = String.format("%064x", new java.math.BigInteger(1, messageDigest.digest()));
+			t.setToken(hashStr);
 		} catch (NoSuchAlgorithmException e) {
+			logger.info("token hash function doesn't exists", e);
 		}
-		messageDigest.update(str.getBytes());
-		String hash_str = String.format("%064x", new java.math.BigInteger(1, messageDigest.digest()));
-		return new Token(hash_str);
+
+		return t;
 	}
+
 	public String getToken() {
-		return token;
+		return tokenString;
 	}
+
 	public void setToken(String token) {
-		this.token = token;
+		this.tokenString = token;
 	}
 }
