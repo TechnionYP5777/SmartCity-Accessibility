a930199c5b1c77f357099f6bea896a468e64ef8a
yaelAmitay <yaelam75@gmail.com>
2017-05-17 21:00:41 +0300

    forgor to add files

---
+++ client-side/src/pages/navigation_maneuver/navigation_maneuver.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams} from 'ionic-angular';
+
+@Component({
+  selector: 'page-navigation_maneuver',
+  templateUrl: 'navigation_maneuver.html'
+}) 
+export class navigationManeuverPage {
+    
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+	}
+	
+	ionViewDidLoad(){
+    }
+}
