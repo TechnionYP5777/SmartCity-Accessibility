f5eede0adf481f7b3707d17d4917ca12e8196f06
Alex Kaplan <alexkaplan.mail@gmail.com>
2017-05-19 18:09:12 +0300

    change ionic port to 80 #344

--- client.sh
+++ client.sh
@@ -6,6 +6,6 @@ cd client-side
 echo "Installing with npm"
 npm install --silent
 echo "Serving ionic"
-screen -d -m -L ionic serve 80 --nolivereload --nobrowser
+screen -d -m -L ionic serve -p 80 --nolivereload --nobrowser
 sleep 5m
 echo "Success!"
