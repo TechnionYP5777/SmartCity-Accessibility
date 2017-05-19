04a9e6f3c592175fe18ef1534a32f8566baa8a22
Alexander Kaplan <alexkaplan.mail@gmail.com>
2017-05-19 13:19:40 +0000

    running client #344

---
+++ client.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Killing previous client if such exists"
+kill -9 `pidof ionic`
+echo "Running client..."
+cd client-side
+echo "Installing with npm"
+npm install --silent
+echo "Serving ionic"
+screen -d -m -L ionic serve --nolivereload --nobrowser
+echo "Success!"
