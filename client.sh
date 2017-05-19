a5916143147a67bb000655ceb5a23907d92a4bbb
Alex Kaplan <alexkaplan.mail@gmail.com>
2017-05-19 17:30:44 +0300

    final scripts (probably) #344

--- client.sh
+++ client.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Killing previous client if such exists"
-kill -9 `pidof ionic`
+killall ionic
 echo "Running client..."
 cd client-side
 echo "Installing with npm"
 npm install --silent
 echo "Serving ionic"
-screen -d -m -L ionic serve --nolivereload --nobrowser
+screen -d -m -L ionic serve 80 --nolivereload --nobrowser
 sleep 5m
 echo "Success!"
--- server.sh
+++ server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd server-side
 echo "Killing previous server"
-kill -9 `cat save_pid.log`
+killall screen
 echo "Running server..."
 screen -dmL mvn exec:java -Dexec.mainClass="smartcity.accessibility.services.Application" -Dserver.port=8090
 sleep 5m
