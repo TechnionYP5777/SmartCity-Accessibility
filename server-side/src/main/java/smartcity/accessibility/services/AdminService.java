c4cbe44a8cd316479d3860fed6e056785f22e560
koralchapnik <chapnikkoral@gmail.com>
2017-05-22 21:16:17 +0300

    helpful users admin servevice #341

--- server-side/src/main/java/smartcity/accessibility/services/AdminService.java
+++ server-side/src/main/java/smartcity/accessibility/services/AdminService.java
@@ -1,21 +1,32 @@
 package smartcity.accessibility.services;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import smartcity.accessibility.database.AbstractUserProfileManager;
+import smartcity.accessibility.database.callbacks.ICallback;
 import smartcity.accessibility.socialnetwork.UserProfile;
 
 @RestController
 public class AdminService {
+	
 	@RequestMapping(value="/adminInfo")
 	@ResponseBody public UserProfile getUserInfo(@RequestHeader("authToken") String token) {	
 		UserInfo userInfo = LogInService.getUserInfo(token);
 		return userInfo.getUser().getProfile();
 	}
 	
-	
+	@RequestMapping("/userHelpful")
+	@ResponseBody public List<UserProfile> getMostHelpfulUsers(@RequestHeader("authToken") String token,
+															   @RequestParam("numOfUsers") Integer numOfUsers) {
+		
+		return AbstractUserProfileManager.instance().mostHelpful(numOfUsers, null);	
+	}
 	
 	
 }
