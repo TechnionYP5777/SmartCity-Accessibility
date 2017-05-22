aa79ef8aa3454310f57afa56ffc9452e25717bfe
Ariel Kolikant <a.kolikant@gmail.com>
2017-05-22 12:17:23 +0300

    #309 added the service for locations in radius

--- client-side/src/pages/location-list/location-list.ts
+++ client-side/src/pages/location-list/location-list.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { LocationsInRadiusService } from './LocationsInRadiusService';
 
 @Component({
   selector: 'page-location-list',
   templateUrl: 'location-list.html'
 })
 export class LocationListPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public locationsInRadius: LocationsInRadiusService) {}
 }
