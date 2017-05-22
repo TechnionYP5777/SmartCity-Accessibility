4bc7ecc0267a9834bcd9646ba54a248e22a4a2bb
Ariel Kolikant <a.kolikant@gmail.com>
2017-05-22 13:46:20 +0300

    #309 some more progress, showing lists is hard apparently

--- client-side/src/pages/location-list/location-list.ts
+++ client-side/src/pages/location-list/location-list.ts
@@ -15,7 +15,10 @@ export class LocationListPage {
 		this.lng = navParams.get('lng');	
 		this.output = "aaaaaaaaaHWERWERWERWER";
 		this.locationsInRadius.GetLocationsInRadiusFrom(this.lat, this.lng).subscribe(data => {
-			this.output = "bbbbbHWERWERWERWER";
+			for (var key in data) {
+				 console.log("we got:", data[key]);
+			}
+			this.output = data[key];
 		});
 	}
 
