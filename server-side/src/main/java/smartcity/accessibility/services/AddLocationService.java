18e40a5b6b1cefbe9fdd0ba15334b283a51090b4
Alex Kaplan <alexkaplan.mail@gmail.com>
2017-05-26 19:02:12 +0300

    minor changes

--- server-side/src/main/java/smartcity/accessibility/services/AddLocationService.java
+++ server-side/src/main/java/smartcity/accessibility/services/AddLocationService.java
@@ -1,6 +1,5 @@
 package smartcity.accessibility.services;
 
-import org.bouncycastle.crypto.tls.SRPTlsClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,14 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.teamdev.jxmaps.LatLng;
 
 import smartcity.accessibility.database.AbstractLocationManager;
-import smartcity.accessibility.database.callbacks.ICallback;
 import smartcity.accessibility.mapmanagement.Location;
-import smartcity.accessibility.mapmanagement.LocationBuilder;
 import smartcity.accessibility.mapmanagement.Location.LocationSubTypes;
 import smartcity.accessibility.mapmanagement.Location.LocationTypes;
+import smartcity.accessibility.mapmanagement.LocationBuilder;
 
 @RestController
-public class addLocationService {
+public class AddLocationService {
 
 	@RequestMapping(value = "/addLocation", method = RequestMethod.POST, produces = "application/json")
 	@ResponseBody
@@ -25,11 +23,6 @@ public class addLocationService {
 			@RequestParam("srcLng") Double srcLng) {
 		Location dummy = new LocationBuilder().setCoordinates(new LatLng(srcLat, srcLng)).setName(name)
 				.setType(LocationTypes.Coordinate).setSubType(LocationSubTypes.Default).build();
-		AbstractLocationManager.instance().uploadLocation(dummy, new ICallback<String>() {
-			@Override
-			public void onFinish(String u) {
-				;
-			}
-		});
+		AbstractLocationManager.instance().uploadLocation(dummy, s -> {});
 	}
 }
