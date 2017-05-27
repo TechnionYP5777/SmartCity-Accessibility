11a9e6fe821df5b576a0d269058665c8dd65084c
yaelAmitay <yaelam75@gmail.com>
2017-05-27 23:40:37 +0300

    add tests to navigation service

--- server-side/src/test/java/smartcity/accessibility/services/NavigationServiceTest.java
+++ server-side/src/test/java/smartcity/accessibility/services/NavigationServiceTest.java
@@ -5,6 +5,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 import java.io.IOException;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -13,6 +14,7 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import smartcity.accessibility.categories.NetworkTests;
 import smartcity.accessibility.database.AbstractLocationManager;
@@ -37,11 +39,14 @@ public class NavigationServiceTest extends ServiceTest {
 		AbstractUserProfileManager.initialize(mock_UserManagerProfile);
 		AbstractLocationManager mock_LocationManagerProfile = Mockito.mock(AbstractLocationManager.class);
 		AbstractLocationManager.initialize(mock_LocationManagerProfile);
-		
+
 		this.mockMvc = webAppContextSetup(webApplicationContext).build();
 		mockMvc.perform(post("/signup?name=me&password=1234"));
 		mockMvc.perform(post("/login?name=me&password=1234"));
-		this.t = Token.calcToken(new UserBuilder().setUsername("me").setPassword("1234").build());//new UserImpl("me", "1234", null));
+		this.t = Token.calcToken(new UserBuilder().setUsername("me").setPassword("1234").build());// new
+																									// UserImpl("me",
+																									// "1234",
+																									// null));
 	}
 
 	@Test
@@ -55,11 +60,48 @@ public class NavigationServiceTest extends ServiceTest {
 		mockMvc.perform(post("/navigation" + "?srcLat=0.0&srcLng=0.0&dstLat=0.0&dstLng=0.0&accessibilityThreshold=0")
 				.header("authToken", this.t.getToken()).contentType(contentType)).andExpect(status().isBadRequest());
 	}
-	
+
 	@Test
 	public void navigationAuthorizedSeccuss() throws IOException, Exception {
-		mockMvc.perform(post("/navigation" + "?srcLat=31.768909&srcLng=34.627724&dstLat=31.771334&dstLng=34.632500&accessibilityThreshold=1")
-				.header("authToken", this.t.getToken()).contentType(contentType)).andExpect(status().is2xxSuccessful());
+		mockMvc.perform(post("/navigation"
+				+ "?srcLat=31.768909&srcLng=34.627724&dstLat=31.771334&dstLng=34.632500&accessibilityThreshold=1")
+						.header("authToken", this.t.getToken()).contentType(contentType))
+				.andExpect(status().is2xxSuccessful());
+	}
+
+	@Test
+	public void responseContainsTime() throws IOException, Exception {
+		mockMvc.perform(post("/navigation"
+				+ "?srcLat=31.768909&srcLng=34.627724&dstLat=31.771334&dstLng=34.632500&accessibilityThreshold=1")
+						.header("authToken", this.t.getToken()).contentType(contentType))
+				.andExpect(status().is2xxSuccessful()).andExpect(MockMvcResultMatchers.jsonPath("$.time").isNumber());
+	}
+
+	@Test
+	public void responseContainsDistance() throws IOException, Exception {
+		mockMvc.perform(post("/navigation"
+				+ "?srcLat=31.768909&srcLng=34.627724&dstLat=31.771334&dstLng=34.632500&accessibilityThreshold=1")
+						.header("authToken", this.t.getToken()).contentType(contentType))
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.distance").isNumber());
+	}
+	
+	@Test
+	public void responseContainsLatLngArray() throws IOException, Exception {
+		mockMvc.perform(post("/navigation"
+				+ "?srcLat=31.768909&srcLng=34.627724&dstLat=31.771334&dstLng=34.632500&accessibilityThreshold=1")
+						.header("authToken", this.t.getToken()).contentType(contentType))
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.latlng").isArray());
+	}
+	
+	@Test
+	public void responseLatLngArrayIsNotEmpty() throws IOException, Exception {
+		mockMvc.perform(post("/navigation"
+				+ "?srcLat=31.768909&srcLng=34.627724&dstLat=31.771334&dstLng=34.632500&accessibilityThreshold=1")
+						.header("authToken", this.t.getToken()).contentType(contentType))
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.latlng").isNotEmpty());
 	}
 
 }
