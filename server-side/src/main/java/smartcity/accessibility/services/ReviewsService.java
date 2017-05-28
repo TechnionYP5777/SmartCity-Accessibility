b90929b616283118a7b2f414896898e6e1056f8b
Arthur Sapozhnikov <depaix3@gmail.com>
2017-05-28 16:52:21 +0300

    Initial server side for likes and dislikes

--- server-side/src/main/java/smartcity/accessibility/services/ReviewsService.java
+++ server-side/src/main/java/smartcity/accessibility/services/ReviewsService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +13,15 @@ import com.teamdev.jxmaps.LatLng;
 
 import smartcity.accessibility.database.AbstractLocationManager;
 import smartcity.accessibility.mapmanagement.Location;
+import smartcity.accessibility.mapmanagement.Location.LocationSubTypes;
+import smartcity.accessibility.mapmanagement.Location.LocationTypes;
 import smartcity.accessibility.socialnetwork.Review;
 import smartcity.accessibility.socialnetwork.UserProfile;
 
 @RestController
 public class ReviewsService {
 	
-	@RequestMapping(value = "/reviews", produces = "application/json")
+	@RequestMapping(value = "/reviews", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
     public Review[] showMeStuff(@RequestParam("lat") Double lat,
     		@RequestParam("lng") Double lng) {
@@ -36,5 +39,15 @@ public class ReviewsService {
 		UserProfile u = new UserProfile("ayyy");
 		return new Review[] { new Review(l, 5, "amazing", u), new Review(l, 3, "so lame bruh", u) };
     }
+	
+	@RequestMapping(value = "/reviews", method = RequestMethod.POST, produces = "application/json")
+	@ResponseBody
+	public void changeRevLikes(@RequestParam("lat") Double lat,
+    		@RequestParam("type") String type,
+    		@RequestParam("subtype") String subtype,
+    		@RequestParam("username") String username,
+    		@RequestParam("likes") Integer like){
+		
+	}
 
 }
