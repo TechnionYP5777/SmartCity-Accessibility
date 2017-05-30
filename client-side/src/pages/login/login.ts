88cbecd0f3fddebb716b150f5324536523753a3c
Alex Kaplan <alexkaplan.mail@gmail.com>
2017-05-30 14:02:13 +0300

    minor fixes after login event

--- client-side/src/pages/login/login.ts
+++ client-side/src/pages/login/login.ts
@@ -3,35 +3,36 @@ import { NavController, Events, LoadingController } from 'ionic-angular';
 import { LoginService } from './LoginService';
 import { HomePage } from '../home/home';
 import { SignupPage } from '../signup/signup';
- 
+import { MapviewPage } from '../mapview/mapview';
+
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
 export class LoginPage {
- 
+
 	usercreds = {
 		 name: '',
 		 password: ''
 	};
 	loading : any;
     constructor(public navCtrl: NavController, public loginservice: LoginService, public events: Events,public loadingCtrl: LoadingController) {}
-  
+
     login(user) {
 		this.presentLoadingCustom();
         this.loginservice.login(user).then(data => {
             if(data) {
 				setTimeout(() => { this.events.publish('login:updateState'); }, this.loginservice.timeout());
 				this.events.publish('login:updateState');
-                this.navCtrl.setRoot(HomePage);
+                this.navCtrl.popToRoot();
 				this.loading.dismiss();
-            } 
+            }
 		});
     }
     signup() {
         this.navCtrl.push(SignupPage);
     }
-	
+
 	presentLoadingCustom() {
             this.loading = this.loadingCtrl.create({
             spinner: 'bubbles',
@@ -41,7 +42,3 @@ export class LoginPage {
         this.loading.present();
     }
 }
-
-
-
-
