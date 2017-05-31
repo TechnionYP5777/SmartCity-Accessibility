c878fa2b957d520d9cb81b089f12c5cf7c497040
Alexander Kaplan <alexkaplan.mail@gmail.com>
2017-05-31 22:55:30 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -3,6 +3,5 @@ cd server-side
 echo "Killing previous server"
 killall screen
 echo "Running server..."
-screen -dmL mvn exec:java -Dexec.mainClass="smartcity.accessibility.services.Application" -Dserver.port=8090
-sleep 5m
+mvn exec:java -Dexec.mainClass="smartcity.accessibility.services.Application" -Dserver.port=8090
 echo "Successs!"
