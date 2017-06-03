d62a946d21184ce9f2b6ac004f521134ccebd6f5
Arthur Sapozhnikov <depaix3@gmail.com>
2017-06-03 13:11:55 +0300

    Update review manager as well #397

--- server-side/src/main/java/smartcity/accessibility/services/ReviewsService.java
+++ server-side/src/main/java/smartcity/accessibility/services/ReviewsService.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.teamdev.jxmaps.LatLng;
 
 import smartcity.accessibility.database.AbstractLocationManager;
+import smartcity.accessibility.database.AbstractReviewManager;
 import smartcity.accessibility.exceptions.UnauthorizedAccessException;
 import smartcity.accessibility.mapmanagement.Location;
 import smartcity.accessibility.mapmanagement.Location.LocationSubTypes;
@@ -84,6 +85,7 @@ public class ReviewsService {
 				try {
 					if (like > 0) r.upvote(u);
 					else r.downvote(u);
+					AbstractReviewManager.instance().updateReview(r, null);
 				} catch (UnauthorizedAccessException e) {
 					throw new UserIsNotLoggedIn();
 				}
