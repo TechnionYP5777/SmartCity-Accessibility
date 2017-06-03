6bb3bc7052fb832a494ac638ddda9088d0e93973
Arthur Sapozhnikov <depaix3@gmail.com>
2017-06-03 13:27:21 +0300

    refacror

--- server-side/src/main/java/smartcity/accessibility/services/ReviewsService.java
+++ server-side/src/main/java/smartcity/accessibility/services/ReviewsService.java
@@ -1,7 +1,5 @@
 package smartcity.accessibility.services;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
