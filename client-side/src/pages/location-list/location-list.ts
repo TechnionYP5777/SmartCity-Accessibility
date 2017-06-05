2411081705045cb6fce54112a15616cb5769810b
Ariel Kolikant <a.kolikant@gmail.com>
2017-06-05 21:36:59 +0300

    made my own kind of loader, can be used from the constroctor close #411

--- client-side/src/pages/location-list/location-list.ts
+++ client-side/src/pages/location-list/location-list.ts
@@ -15,19 +15,20 @@ export class LocationListPage {
 	output2: any;
 	locations: any;
 	location: any;
-	youChose: any;
+	pleaseWait: any;
 	index: any;
 	constructor(public viewCtrl: ViewController, public modalCtrl: ModalController, public navCtrl: NavController, public navParams: NavParams, public locationsInRadius: LocationsInRadiusService) {
 		this.lat = navParams.get('lat');
 		this.lng = navParams.get('lng');	
 		this.output = "Calculating...";
-		this.youChose = "you chose: "
-		this.locations = [{"reviews":[],"coordinates":{"lng":0,"lat":0},"name":"please wait...","locationType":"Coordinate","locationSubType":"Default","notPinnedReviews":[],"pinnedReviews":[]}];
+		this.pleaseWait = true;
+		this.locations = [];
 		this.locationsInRadius.GetLocationsInRadiusFrom(this.lat, this.lng).subscribe(data => {	
 			for(var count = 0; count < data.length; count++){
 				this.locations[count] = data[count];
 			}
 		this.output = this.locations;
+		this.pleaseWait = false;
 		});
 	}
 
