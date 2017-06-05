d9584735e81a58889752d470848251289615d06b
Ariel Kolikant <a.kolikant@gmail.com>
2017-06-05 22:55:43 +0300

    solved it, close #399

--- server-side/src/main/java/smartcity/accessibility/services/LocationsInRadiusService.java
+++ server-side/src/main/java/smartcity/accessibility/services/LocationsInRadiusService.java
@@ -1,6 +1,9 @@
 package smartcity.accessibility.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,7 @@ import smartcity.accessibility.database.AbstractLocationManager;
 import smartcity.accessibility.exceptions.illigalString;
 import smartcity.accessibility.mapmanagement.JxMapsFunctionality.ExtendedMapView;
 import smartcity.accessibility.mapmanagement.Location;
+import smartcity.accessibility.mapmanagement.LocationBuilder;
 import smartcity.accessibility.search.SearchQuery;
 import smartcity.accessibility.search.SearchQueryResult;
 /*
@@ -25,28 +29,40 @@ import smartcity.accessibility.search.SearchQueryResult;
 //localhost:8090/locationsInRadius?srcLat=39.9129266808733&srcLng=-104.7956711
 @RestController
 public class LocationsInRadiusService {
-	private static final double radius = 0.001;
+	private static final double Radius = 0.001;
 	@RequestMapping(value="/locationsInRadius")
 	@ResponseBody public List<Location> getLocationsInRadius(@RequestHeader("authToken") String token, @RequestParam("srcLat") Double srcLat,
 			@RequestParam("srcLng") Double srcLng) {	
-		//ExtendedMapView mv = LogInService.getUserInfo(token).getMapView();
 		try {
 			ExtendedMapView mapView = LogInService.getMapView(token);
 			synchronized(mapView){
 				SearchQuery sq = SearchQuery.TypeSearch("", mapView);
 				Location dummy = new Location();
 				dummy.setCoordinates(new LatLng(srcLat, srcLng));
-				SearchQueryResult sqr = sq.searchByType(dummy, radius);
-				List<Location> retVal = AbstractLocationManager.instance().getLocationsAround(new LatLng(srcLat, srcLng), radius, null);	
+				SearchQueryResult sqr = sq.searchByType(dummy, Radius);
+				List<Location> retVal = AbstractLocationManager.instance().getLocationsAround(new LatLng(srcLat, srcLng), Radius, null);	
 				sq.waitOnSearch();
-				retVal.addAll(sqr.getLocations());
-				return retVal;	
+				return preferDBLocations(sqr.getLocations(), retVal);
 			}
 		} catch (illigalString | InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 			return null;
 		} 
+	}
+	
+	List<Location> preferDBLocations(List<Location> fromGM, List<Location> fromDB){
+		List<Location> DBasDefualtValues = new ArrayList<>();
+		DBasDefualtValues.addAll(fromDB);
+		DBasDefualtValues.stream().map(new Function<Location, Location>() {
+			@Override
+			public Location apply(Location arg0) {
+				return new LocationBuilder().setCoordinates(arg0.getCoordinates()).setName(arg0.getName())
+				.setType(Location.LocationTypes.Coordinate).setSubType(Location.LocationSubTypes.Default).build();
+			}
+        });
+		List<Location> retVal = new ArrayList<>();
+		retVal.addAll(fromGM.stream().filter(p -> !DBasDefualtValues.contains(p)).collect(Collectors.toList()));
+		retVal.addAll(fromDB);
+		return retVal;
 	}
 
 }
