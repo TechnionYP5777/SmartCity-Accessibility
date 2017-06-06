cfadf678eef67fde03ff3e64e839748ea4bb20ee
Alexander Kaplan <alexkaplan.mail@gmail.com>
2017-06-06 20:36:11 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-heroku auth:token
-heroku open -a smartcity-accessibility
+heroku git:remote -a smartcity-accessibility
 heroku config:set MAVEN_CUSTOM_GOALS="clean package spring-boot:repackage"
 git subtree push --prefix server-side heroku master
