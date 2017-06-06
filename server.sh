c017435b504614377e18012255611f899db4fd5d
Alexander Kaplan <alexkaplan.mail@gmail.com>
2017-06-06 16:39:56 +0000

    server.sh and port #413

--- server.sh
+++ server.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-cd server-side
-echo "Killing previous server"
-killall screen
-echo "Running server..."
-screen -dmL mvn clean install exec:java -Dexec.mainClass="smartcity.accessibility.services.Application" -Dserver.port=8090 -DskipTests
-sleep 5m
+git subtree push --prefix server-side heroku master
 echo "Successs!"
