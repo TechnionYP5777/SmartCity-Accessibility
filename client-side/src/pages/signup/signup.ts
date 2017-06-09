f7d8ddfdd79e727a6497da330ce2e79ea7fddc8e
yaelAmitay <yaelam75@gmail.com>
2017-06-09 13:31:23 +0300

    add loading spinner to sign up

--- client-side/src/pages/signup/signup.ts
+++ client-side/src/pages/signup/signup.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, Events, AlertController } from 'ionic-angular';
+import { NavController, Events, AlertController, LoadingController } from 'ionic-angular';
 import { LoginService } from '../login/LoginService';
 
 
@@ -12,9 +12,12 @@ export class SignupPage {
 	    name: '',
 		password: ''
 	};
-  constructor(public navCtrl: NavController, public loginService: LoginService,public events: Events, public alertCtrl: AlertController) {}
+  
+  loading : any;
+  constructor(public navCtrl: NavController, public loginService: LoginService,public loadingCtrl: LoadingController,public events: Events, public alertCtrl: AlertController) {}
 
   register(user) {
+		this.presentLoadingCustom();
         this.loginService.signup(user).then(data => {
             if(data) {
                 var alert = this.alertCtrl.create({
@@ -25,8 +28,20 @@ export class SignupPage {
 				setTimeout(() => { this.events.publish('login:updateState'); }, this.loginService.timeout());
 				this.events.publish('login:updateState');
                 alert.present();
+				this.loading.dismiss();
                 this.navCtrl.popToRoot();
-            }
+            } else {
+				this.loading.dismiss();
+			}
     });
-}
+	}
+	
+	presentLoadingCustom() {
+            this.loading = this.loadingCtrl.create({
+            spinner: 'bubbles',
+		    showBackdrop: false,
+		    cssClass: 'loader'
+        });
+        this.loading.present();
+    }
 }
