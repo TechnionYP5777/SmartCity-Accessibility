df63f25af614aec0f8ef3a813ebf086aae171683
yaelAmitay <yaelam75@gmail.com>
2017-06-10 23:40:30 +0300

    add server side service to get image pofile #424

---
+++ server-side/src/main/java/smartcity/accessibility/services/ProfileImageService.java
@@ -0,0 +1,51 @@
+package smartcity.accessibility.services;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 
+ * @author yael
+ *
+ */
+@Controller
+public class ProfileImageService {
+	private static Logger logger = LoggerFactory.getLogger(ProfileImageService.class);
+
+	@RequestMapping(value = "/profileImg", method = RequestMethod.GET)
+	public void getImageNerrative(HttpServletResponse response, @RequestParam String UserName) throws IOException {
+		ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+		try {
+			BufferedImage image = ImageIO.read(new File("")); // TODO get image
+																// instead of ""
+			ImageIO.write(image, "jpeg", pngOutputStream);
+		} catch (IllegalArgumentException e) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			logger.info("problem reading image",e);
+		}
+
+		byte[] imgByte = pngOutputStream.toByteArray();
+
+		response.setHeader("Cache-Control", "no-store");
+		response.setHeader("Pragma", "no-cache");
+		response.setDateHeader("Expires", 0);
+		response.setContentType("image/png");
+		ServletOutputStream responseOutputStream = response.getOutputStream();
+		responseOutputStream.write(imgByte);
+		responseOutputStream.flush();
+		responseOutputStream.close();
+	}
+}
