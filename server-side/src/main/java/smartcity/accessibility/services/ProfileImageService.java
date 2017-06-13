0dfc4915597d7c4a3231b3a097ccf8b23baa9ce6
yaelAmitay <yaelam75@gmail.com>
2017-06-13 20:53:08 +0300

    add imports

--- server-side/src/main/java/smartcity/accessibility/services/ProfileImageService.java
+++ server-side/src/main/java/smartcity/accessibility/services/ProfileImageService.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
