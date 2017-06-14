3a52390f8385f1d460be46b558dd69d272f29ab1
Arthur Sapozhnikov <depaix3@gmail.com>
2017-06-14 23:38:54 +0300

    Add review also shows address instead of LatLng

--- client-side/src/pages/add-review/add-review.ts
+++ client-side/src/pages/add-review/add-review.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {AddReviewService} from './AddReviewService';
 import { LoginService } from '../login/LoginService';
-
+import { SearchService } from '../mapview/searchService';
 
 
 @Component({
@@ -18,13 +18,18 @@ export class AddReviewPage {
   subtype : any;
   name : any;
   streetReview : boolean;
+  address : any;
 
   reviewinfo = {
 		 review: '',
 		 score: ''
 	};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public addreviewservice: AddReviewService, public loginService : LoginService) {
+  constructor(public navCtrl: NavController,
+			public navParams: NavParams,
+			public addreviewservice: AddReviewService,
+			public loginService : LoginService,
+			public searchService : SearchService) {
     this.token = window.sessionStorage.getItem('token');
 	this.lat = navParams.get('lat');
 	this.lng = navParams.get('lng');
@@ -32,6 +37,9 @@ export class AddReviewPage {
 	this.subtype = navParams.get('subtype');
 	this.name = navParams.get('name');
 	this.streetReview = false;
+	this.searchService.getAdress(this.lat, this.lng).subscribe(data => {	
+		this.address = data.res;
+	});
   }
 
   starClicked(value){
