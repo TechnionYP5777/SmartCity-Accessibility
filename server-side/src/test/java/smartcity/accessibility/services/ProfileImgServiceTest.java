82a674eefc74b6bcbe8b9dc435befc89daa7abee
yaelAmitay <yaelam75@gmail.com>
2017-06-14 22:53:12 +0300

    add segment id to reviews on street #433

--- server-side/src/test/java/smartcity/accessibility/services/ProfileImgServiceTest.java
+++ server-side/src/test/java/smartcity/accessibility/services/ProfileImgServiceTest.java
@@ -20,8 +20,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-
 import smartcity.accessibility.categories.UnitTests;
 import smartcity.accessibility.database.AbstractUserProfileManager;
 import smartcity.accessibility.database.ParseDatabase;
