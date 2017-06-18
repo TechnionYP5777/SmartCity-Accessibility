a70b382e8a3ddbe09473767d270631bbfad2c66d
koralchapnik <chapnikkoral@gmail.com>
2017-06-19 00:04:10 +0300

    add another test #321

--- server-side/src/test/java/smartcity/accessibility/services/ComplexSearchServiceTest.java
+++ server-side/src/test/java/smartcity/accessibility/services/ComplexSearchServiceTest.java
@@ -1,5 +1,6 @@
 package smartcity.accessibility.services;
 
+import static org.junit.Assert.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
@@ -13,7 +14,6 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import smartcity.accessibility.categories.UnitTests;
 import smartcity.accessibility.database.AbstractLocationManager;
@@ -50,11 +50,19 @@ public class ComplexSearchServiceTest extends ServiceTest {
 	
 	@Test
 	@Category(UnitTests.class)
-	public void checkComplexSearch() throws Exception {
+	public void checkSuccess() throws Exception {
 		mockMvc.perform(post("/complexSearchAddress?type=restaurant&radius=1000&startLocation=haifa&threshold=1")
 				.header("authToken", this.t.getToken()).contentType(contentType))
 		.andExpect(status().is2xxSuccessful());	
 	}
 	
-	
+	@Test
+	@Category(UnitTests.class)
+	public void checkFailure() throws Exception {
+		try {
+			mockMvc.perform(post("/complexSearchAddress?type=restaurant&radius=1000&startLocation=&threshold=1")
+				.header("authToken", this.t.getToken()).contentType(contentType));	
+			fail("shouldnt get here");
+		} catch (Exception e) {}
+	}
 }
