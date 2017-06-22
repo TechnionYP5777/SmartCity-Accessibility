c56eb13d09d59b94de80f06f8a00adbadbfccf4d
yaelAmitay <yaelam75@gmail.com>
2017-06-23 01:30:10 +0300

    return to localhost

--- client-side/src/pages/constants.ts
+++ client-side/src/pages/constants.ts
@@ -1,4 +1,4 @@
 export class Constants {
-	public static serverAddress='http://192.168.1.19:8080';
+	public static serverAddress='http://localhost:8080';
 	public static userExpiredMessage = "Sorry, it seems you were not active for 30 minutes. Please re-login.";
 }
