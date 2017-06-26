61ff1055999c6c56df76de743d207b00a261ea6f
Arthur Sapozhnikov <depaix3@gmail.com>
2017-06-26 20:47:08 +0300

    A lot of refactoring. Added server side of (un)pin and delete reviews.

--- server-side/src/main/java/smartcity/accessibility/services/ReviewsService.java
+++ server-side/src/main/java/smartcity/accessibility/services/ReviewsService.java
@@ -17,6 +17,7 @@ import smartcity.accessibility.exceptions.UnauthorizedAccessException;
 import smartcity.accessibility.mapmanagement.Location;
 import smartcity.accessibility.mapmanagement.Location.LocationSubTypes;
 import smartcity.accessibility.mapmanagement.Location.LocationTypes;
+import smartcity.accessibility.services.exceptions.LocationDoesNotExistException;
 import smartcity.accessibility.services.exceptions.UserDoesNotExistException;
 import smartcity.accessibility.services.exceptions.UserIsNotLoggedIn;
 import smartcity.accessibility.socialnetwork.Review;
@@ -26,6 +27,27 @@ import smartcity.accessibility.socialnetwork.User;
 public class ReviewsService {
 	private static Logger logger = LoggerFactory.getLogger(ReviewsService.class);
 	
+	private Location createLocation(Double lat, Double lng, String type, String subtype) {
+		return AbstractLocationManager.instance().
+				getLocation(new LatLng(lat, lng),
+				LocationTypes.valueOf(type.toUpperCase()),
+				LocationSubTypes.valueOf(subtype.toUpperCase()),
+				null).orElse(null);
+	}
+	
+	private Review getUserReviewFromLocation(String username, Location loc) {
+		if(loc == null)
+			throw new LocationDoesNotExistException();
+		
+		for(Review r :loc.getReviews()){
+			if (r.getUser().getUsername().equals(username)){
+				r.setLocation(loc);
+				return r;
+			}
+		}
+		return null;
+	}
+	
 	@RequestMapping(value = "/reviews", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
     public Review[] showMeStuff(@RequestParam("lat") Double lat,
@@ -34,11 +56,7 @@ public class ReviewsService {
 			@RequestParam("subtype") String subtype,
 			@RequestParam("name") String name) {
 		
-		Location loc = AbstractLocationManager.instance().
-				getLocation(new LatLng(lat, lng),
-				LocationTypes.valueOf(type.toUpperCase()),
-				LocationSubTypes.valueOf(subtype.toUpperCase()),
-				null).orElse(null);
+		Location loc = createLocation(lat, lng, type, subtype);
 		
 		if(loc == null){ //Location "l" doesn't exist
 			loc = new Location();
@@ -66,27 +84,46 @@ public class ReviewsService {
 		if (u == null)
 			throw new UserDoesNotExistException();
 		
-		LatLng coords = new LatLng(lat, lng);
-		Location loc = AbstractLocationManager.instance().getLocation(
-				coords,
-				LocationTypes.valueOf(type.toUpperCase()),
-				LocationSubTypes.valueOf(subtype.toUpperCase()),
-				null).orElse(null);
-			
+		Review r = getUserReviewFromLocation(username, createLocation(lat, lng, type, subtype));
+		try {
+			if (like > 0) r.upvote(u);
+			else r.downvote(u);
+			AbstractReviewManager.instance().updateReview(r, null);
+		} catch (UnauthorizedAccessException e) {
+			logger.error("{}", e);
+			throw new UserIsNotLoggedIn();
+		}
+	}
 
-		for(Review r :loc.getReviews())
-			if (r.getUser().getUsername().equals(username)){
-				r.setLocation(loc);
-				try {
-					if (like > 0) r.upvote(u);
-					else r.downvote(u);
-					AbstractReviewManager.instance().updateReview(r, null);
-				} catch (UnauthorizedAccessException e) {
-					logger.error("{}", e);
-					throw new UserIsNotLoggedIn();
-				}
-				break;
-			}
+	
+	@RequestMapping(value = "/deleteReview", method = RequestMethod.POST, produces = "application/json")
+	@ResponseBody
+	public void deleteReview(@RequestHeader("authToken") String token,
+			@RequestParam("lat") Double lat,
+			@RequestParam("lng") Double lng,
+    		@RequestParam("type") String type,
+    		@RequestParam("subtype") String subtype,
+    		@RequestParam("username") String username){
+		
+		Review r = getUserReviewFromLocation(username, createLocation(lat, lng, type, subtype));
+		
+		AbstractReviewManager.instance().deleteReview(r, null);
+	}
+	
+	@RequestMapping(value = "/pinUnpinReview", method = RequestMethod.POST, produces = "application/json")
+	@ResponseBody
+	public void pinUnpinReview(@RequestHeader("authToken") String token,
+			@RequestParam("lat") Double lat,
+			@RequestParam("lng") Double lng,
+    		@RequestParam("type") String type,
+    		@RequestParam("subtype") String subtype,
+    		@RequestParam("username") String username){
+		
+		Review r = getUserReviewFromLocation(username, createLocation(lat, lng, type, subtype));
+		
+		r.setPinned(!r.isPinned());
+		
+		AbstractReviewManager.instance().updateReview(r, null);
 	}
 
 }
